76cf7a735dc67cdf7558656422fec12cc8c4642b
Abdullahi Ismail <79646763+abdiulismail@users.noreply.github.com>
2022-07-22 20:18:18 +0300

    created models and handlers

--- README.md
+++ README.md
@@ -8,10 +8,5 @@ BankingAPI is a Go application that implements simple banking features
 [GorillaMux](https://github.com/gorilla/mux/)
 
 
-## Contributing
-Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
-
-Please make sure to update tests as appropriate.
-
 ## License
 [MIT](https://choosealicense.com/licenses/mit/)
--- handlers/getAllCustomers.go
+++ handlers/getAllCustomers.go
@@ -8,11 +8,12 @@ import (
 
 func GetAllCustomers(w http.ResponseWriter, r *http.Request) {
 	customers := []models.Customer{
-		{"John","Nairobi","1234"},
-		{"Alex","Lisbon","345"},
+		{"John", "Nairobi", "1234"},
+		{"Alex", "Lisbon", "345"},
 	}
-	w.Header().Add("Content-Type","application/json")
-	json.NewEncoder(w).Encode(customers)
+
+	w.Header().Add("Content-Type", "application/json")
+	_ = json.NewEncoder(w).Encode(customers)
 	//res, _ := json.Marshal(customers)
 	//w.Write(res)
 }
